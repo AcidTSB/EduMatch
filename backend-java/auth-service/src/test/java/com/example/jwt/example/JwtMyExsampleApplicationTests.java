a2cf765b4d2a4b5e57811bf073f1e06eeaa099c4
hunter875 <minhln8a6@gmail.com>
2025-11-25 12:49:03 -0800

    fix: use @EnableAutoConfiguration to exclude RabbitMQ

--- backend-java/auth-service/src/test/java/com/example/jwt/example/JwtMyExsampleApplicationTests.java
+++ backend-java/auth-service/src/test/java/com/example/jwt/example/JwtMyExsampleApplicationTests.java
@@ -1,11 +1,13 @@
 package com.example.jwt.example;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(exclude = RabbitAutoConfiguration.class)
+@SpringBootTest
+@EnableAutoConfiguration(exclude = RabbitAutoConfiguration.class)
 @ActiveProfiles("test")
 class JwtMyExsampleApplicationTests {
 
