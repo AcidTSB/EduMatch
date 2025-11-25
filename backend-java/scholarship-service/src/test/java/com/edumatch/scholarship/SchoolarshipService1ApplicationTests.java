6da15d23079ad8bcde3cdaa247054059e0d402af
hunter875 <minhln8a6@gmail.com>
2025-11-25 12:35:37 -0800

    fix: add H2 database and test configs for all services

--- backend-java/scholarship-service/src/test/java/com/edumatch/scholarship/SchoolarshipService1ApplicationTests.java
+++ backend-java/scholarship-service/src/test/java/com/edumatch/scholarship/SchoolarshipService1ApplicationTests.java
@@ -2,8 +2,10 @@ package com.edumatch.scholarship;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SchoolarshipService1ApplicationTests {
 
     @Test
